36ac9a5ddc73d2283e84ef3492a51b0274c76b76
Alan <alangomez.ba@gmail.com>
2022-04-12 20:14:24 -0300

    clase javascript version 1

---
+++ script.js
@@ -0,0 +1,70 @@
+//esto es un alert, comentario de una linea//
+//alert("hola mundo")
+
+
+/*todo
+es
+comentario multilinea
+*/
+// let te avisa los errores (solo se puede usar una vez para una variable), var no te avisa//
+let alumno = "Fede" // creo la variable//
+let nota = 9
+
+alumno = "Alan" //le cambio el contenido//
+
+alumno = "fede"
+
+const entidad = "gobierno ciudad"
+
+console.log( "el nombre del alumno es " + alumno + " y su nota es " + nota) // la imprimo en pantalla//
+
+//VAN
+let nombreApellido = "juan batman"
+let User = "Juan Carlos"
+let _usuario 
+let $user
+let nombre_apellido = "Snake case"
+let NombreApellido
+
+//No Van
+/*let 1ernombre
+let nombre-apellido
+let var super break this*/
+
+///////STRING
+let str1 = "Juan"
+let str2 = 'Pedro'
+
+let str4 = str1 + " y " + str2
+let str5 = '${str1} y ${str2}'
+
+
+
+
+console.log (str1 + " y " + str2)
+
+console.log (str4)
+console.log (str5)
+
+
+/////NUMBER
+
+let num1 = parseFloat(prompt("mete el primer numero"))
+let num2 = parseFloat(prompt("mete el segundo numero"))
+
+
+//operaciones basicas
+let resta = num1 - num2
+let suma = num1 + num2
+let mult = num1 * num2
+let division = num1/num2
+
+console.log("la resta es:"+resta)
+console.log (num1 + num2)
+console.log("La suma es :"+suma)
+console.log("la multiplicacion es :"+mult)
+
+
+
+document.write("<h1> Javascript </h1>" + mult);
+
